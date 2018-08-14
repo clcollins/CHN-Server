3d700f03e0130f61814711fd1fcdda9fff8fb1be
Christopher Collins <collins.christopher@gmail.com>
2018-08-14 15:49:59 -0400

    moving initdatabase.py to be the docker entrypoint for uwsgi
    Signed-off-by: Christopher Collins <collins.christopher@gmail.com>

--- init_uwsgi.py
+++ init_uwsgi.py
@@ -5,12 +5,18 @@ from sqlalchemy import inspect
 import sys
 
 if __name__ == '__main__':
+
+    uwsgi_init = ['uwsgi',
+                  '--http 8080',
+                  '--module mhn:mhn',
+                  '--buffer-size 04096']
+
     with mhn.test_request_context():
         inspector = inspect(db.engine)
         if 'user' in inspector.get_table_names():
             print("Database already initialized")
-            sys.exit()
         else:
             print("Initializing new database")
             create_clean_db()
 
+    os.execv('uwsgi', uwsgi_init)
